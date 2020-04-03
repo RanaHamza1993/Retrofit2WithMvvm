a03a91370297959e4a9b2f9cf1a80c0b82b2f8be
hackerrank <me@hackerrank.com>
2020-04-03 14:58:03 +0500

    push

--- app/src/main/java/com/hamza/retrofit2withmvvm/generics/Couroutines.kt
+++ app/src/main/java/com/hamza/retrofit2withmvvm/generics/Couroutines.kt
@@ -11,12 +11,12 @@ object Couroutines {
             work()
         }
 
-    fun <T : Any> ioThenMain(work: suspend () -> T, callback: (T?) -> Unit) {
+    fun <T : Any> ioThenMain(work: suspend () -> T, callback: (T?) -> Unit) =
         CoroutineScope(Dispatchers.Main).launch {
             val data = CoroutineScope(Dispatchers.IO).async rt@{
                 return@rt work()
             }.await()
             callback(data)
         }
-    }
+
 }
