fa0e6ca9622c73b1245e1e624b2fd0331ae3b002
hackerrank <me@hackerrank.com>
2020-04-03 14:55:51 +0500

    push

--- app/src/main/java/com/hamza/retrofit2withmvvm/generics/Couroutines.kt
+++ app/src/main/java/com/hamza/retrofit2withmvvm/generics/Couroutines.kt
@@ -2,11 +2,21 @@ package com.hamza.retrofit2withmvvm.generics
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 object Couroutines {
-    fun main(work:suspend(()->Unit)) =
+    fun main(work: suspend (() -> Unit)) =
         CoroutineScope(Dispatchers.Main).launch {
             work()
         }
+
+    fun <T : Any> ioThenMain(work: suspend () -> T, callback: (T?) -> Unit) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val data = CoroutineScope(Dispatchers.IO).async rt@{
+                return@rt work()
+            }.await()
+            callback(data)
+        }
+    }
 }
